5049b8c9551b725bd84cd128cc64ace663db1526
ponni12345 <ponnisivakumar335@gmail.com>
2024-12-24 06:49:51 -0800

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,19 @@
+#print
+#string
+print("hello")
+print("hello")
+#assign string to a vaiable
+a="hello"
+print(a)
+#multline string
+a='''loren ipsum dolor sit amet, consection adipicing elit, sed to eiusmad tempor ncicidiunt ut labore t dolore maganaalique.'''
+print(a)
+#string are array
+a="hello,world"
+print(a[1])
+#length function
+a="hello,world"
+print(len(a))
+#check funtion
+txt="the best things in lfe are free!"
+print("best"in txt)
