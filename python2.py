df4143d70e677941841f1a149597d05fea480c58
ponni12345 <ponnisivakumar335@gmail.com>
2024-12-24 06:50:34 -0800

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,28 @@
+#Interer
+x=1
+y=12345
+z=-34567
+print(type(x))
+print(type(y))
+print(type(z))
+#float
+x=0.10
+y=1.0
+z=-35.59
+print(type(x))
+print(type(y))
+print(type(z))
+#float with e,E to indicate the power of 10
+x=35e3
+z=-87.7e100
+print(type(x))
+print(type(z))
+#complex j as the imginary part
+x=3+5j
+y=5j
+a=-2j
+print(type(x))
+print(type(y))
+print(type(a))
+
+
