b66d4f8b8fb5ebf2da599fa19882f8262c64ac34
ponni12345 <ponnisivakumar335@gmail.com>
2024-12-24 06:43:35 -0800

    Add files via upload

---
+++ python8.py
@@ -0,0 +1,7 @@
+sample_string="hello,world!"
+print(sample_string.capitalize())
+#lower case
+print(sample_string.casefold())
+print(sample_string.center(20,'_'))
+print(sample_string.count("l"))
+print(sample_string.encode(encoding="utf-8"))
