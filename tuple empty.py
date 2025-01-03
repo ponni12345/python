44c0b9302f41e441f076e41e60a2b23f642d1f84
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-03 14:41:05 +0530

    Add files via upload

---
+++ tuple empty.py
@@ -0,0 +1,26 @@
+#tuple empty and single
+empty=()
+singleton='hello',
+print(len(empty))
+print(len(singleton))
+print(singleton)
+print(type(singleton))
+t=123,543,'tuple'
+x,y,z=t
+print(t)
+print(x)
+print(y)
+print(z)
+#tuple empty and single
+empty=()
+singleton='hello'
+print(len(empty))
+print(len(singleton))
+print(singleton)
+print(type(singleton))
+t=123,543,'tuple'
+x,y,z=t
+print(x)
+print(y)
+print(z)
+
