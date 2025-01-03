4e43931b5c418c6dbb7499faf29d1d26f64a74d2
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-03 14:43:10 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+#dictionaries
+dic={'name':'ram','age':26}
+print(dic['name'])
+a={}
+print(type(a))
+dic['guide']=987
+print(dic)
+del dic['age']
+print(dic)
+print(list(dic))
+print(sorted(dic))
+print('guido'in dic)
+print('guide'in dic)
+dic={'name':'sam','age':20,'mark':90}
+print(dic['name'])
+a={}
+print(type(a))
+dic['guide']=987
+print(dic)
+del dic['age']
+print(dic)
+print(list(dic))
+print(sorted(dic))
+print('guido'in dic)
+print('guide'in dic)
