50792e34b7acab96216ecfc5ef1303b3a2f06926
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-04 20:22:28 +0530

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,31 @@
+
+a=input("enter the name:")
+b=input("enter the email:")
+c=input("enter the phone no:")
+print(a)
+print(b)
+print(c)
+
+a={'name':'sam'}
+b={'email':'siva123'}
+c={'phone':'123457891'}
+print(a)
+print(b)
+print(c)
+
+a='name:'
+b='sam'
+c=a+b
+print(c)
+d='email:'
+e='siva123@gmail.com'
+f=d+e
+print(f)
+x='phone:'
+y='1234567891'
+z=x+y
+print(z)
+ 
+
+
+
