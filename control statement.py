3937a643ae892ae47388943663c62da08b65038d
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-07 19:43:01 +0530

    Add files via upload

---
+++ control statement.py
@@ -0,0 +1,27 @@
+
+#simple for
+for x in range(30):
+    print("we are on times",x)
+
+#for and break
+for x in range(30):
+  if x==4:
+     print("this is four")
+     break
+  print(x)
+
+#for else
+for x in range(7):
+     print(x)
+else:
+     print("loop ended")
+
+#for using string
+string="hello world"
+for x in string:
+     print(x)
+#collection in for loop
+collection=["hey",5,"d"]
+for x in collection:
+     print(x)
+                
