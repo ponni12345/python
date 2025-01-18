5902e8e6d804e41b8df5cf80c8b4d0b7ac572e22
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-18 19:27:13 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,12 @@
+tree=['root',
+      ['left',
+       ['left.left',[]],
+       ['left.right',[]],
+        ],
+      ['right',
+       ['right.left',[]],
+       ['right.right',[]],
+       ]
+      ]
+print("\n tree representation:")
+print(tree)
