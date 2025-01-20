93b5e190fadeae1a85bb33bf7a6680fcdf451ec8
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-20 20:08:21 +0530

    Add files via upload

---
+++ my_function.py
@@ -0,0 +1,9 @@
+def my_function(**sum):
+    print("the sum of the ",sum['fname'],sum['lname'])
+my_function(fname="two",lname="number")
+def my_function(number="division"):
+    print("the addition is "+number)
+my_function("addition")
+my_function("subration")
+my_function()
+my_function("modules")
