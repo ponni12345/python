dd79327188d6b5cbb63c35b34c2a1da9fa5a34de
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-20 19:07:02 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,20 @@
+def func():
+    print("something")
+    print("so this is function")
+    print("this is very useful for reduce code")
+func()
+func()
+
+
+def greet(name):
+    print("hi"+name+",welcme to livewire!")
+greet("raja")
+b="bala"
+greet(b)
+
+
+def name(first,second):
+    print(first+second)
+name("rajendra","bala")    
+
+
