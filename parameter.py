a12c0587e874e6f849723c712ef41e34f9761d02
ponni12345 <ponnisivakumar335@gmail.com>
2025-01-20 19:08:34 +0530

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("this is "+name+"")
+greet("arun")
+def greet(age):   
+    print("i am "+age+" years")
+greet("20")
+def greet(department):    
+    print("department in "+department+"")
+greet("BSC COMPUTER SCIENCE")
+def greet(course):    
+    print("course name is "+course+"")
+greet("JAVA")
