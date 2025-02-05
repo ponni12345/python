26a20f25fad70b4571965b90b30fd3e276182a16
ponni12345 <ponnisivakumar335@gmail.com>
2025-02-05 14:44:09 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,21 @@
+# open("file.txt","x")
+
+f=open("file.txt","w")
+f.write("\nsomething213874")
+
+f=open("file.txt","a")
+f.write("\nsomething213874")
+
+f=open("file.txt","a")
+f.write("\nname:ponni")
+
+f=open("file.txt","a")
+f.write("\nmark:500")
+
+f=open("file.txt","a")
+f.write("\nage:21")
+
+f=open("file.txt","r")
+print(f.read())
+
+
