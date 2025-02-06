4f742cbdf2fd8c654d18f9cda0a2107e168480ae
ponni12345 <ponnisivakumar335@gmail.com>
2025-02-06 14:36:26 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,97 @@
+#open("file 1.txt","x")
+
+f=open("file 1.txt","w")
+f.write("\nemployee 1")
+
+f=open("file 1.txt","a")
+f.write("\nname:arun")
+
+f=open("file 1.txt","a")
+f.write("\ncompany:ZOHO")
+
+f=open("file 1.txt","a")
+f.write("\nexperience:2year")
+
+f=open("file 1.txt","a")
+f.write("\nsalary:20000")
+
+f=open("file 1.txt","r")
+print(f.read())
+
+f=open("file 1.txt","w")
+f.write("\nemployee 2")
+
+f=open("file 1.txt","a")
+f.write("\nname:deepak")
+
+f=open("file 1.txt","a")
+f.write("\ncompany:microsoft")
+
+f=open("file 1.txt","a")
+f.write("\nexperience:3year")
+
+f=open("file 1.txt","a")
+f.write("\nsalary:20000")
+
+f=open("file 1.txt","r")
+print(f.read())
+
+f=open("file 1.txt","w")
+f.write("\nemployee 3")
+
+f=open("file 1.txt","a")
+f.write("\nname:ponni")
+
+f=open("file 1.txt","a")
+f.write("\ncompany:google")
+
+f=open("file 1.txt","a")
+f.write("\nexperience:2year")
+
+f=open("file 1.txt","a")
+f.write("\nsalary:25000")
+
+f=open("file 1.txt","r")
+print(f.read())
+
+f=open("file 1.txt","w")
+f.write("\nemployee 4")
+
+
+f=open("file 1.txt","a")
+f.write("\nname:raj")
+
+f=open("file 1.txt","a")
+f.write("\ncompany:cisco")
+
+f=open("file 1.txt","a")
+f.write("\nexperience:2year")
+
+f=open("file 1.txt","a")
+f.write("\nsalary:25000")
+
+f=open("file 1.txt","r")
+print(f.read())
+
+
+f=open("file 1.txt","w")
+f.write("\nemployee 5")
+
+
+f=open("file 1.txt","a")
+f.write("\nname:babu")
+
+f=open("file 1.txt","a")
+f.write("\ncompany:IBM")
+
+f=open("file 1.txt","a")
+f.write("\nexperience:4year")
+
+f=open("file 1.txt","a")
+f.write("\nsalary:35000")
+
+f=open("file 1.txt","r")
+print(f.read())
+
+
+
