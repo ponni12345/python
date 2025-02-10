f307f97e6ef311a9de2f7641eace92f3bd359dbd
ponni12345 <ponnisivakumar335@gmail.com>
2025-02-10 14:54:06 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,20 @@
+name=input("enter the name:")
+age=input("enter your age:")
+place=input("enter your place:")
+file=open("studentdetail.txt","a")
+file.write("\n"+name+"\t"+age+"\t"+place)
+
+name=input("enter your name:")
+place=input("enter your place:")
+
+
+file=open("studentdetail.txt","r")
+info=file.read().split("\n")
+for data in info:
+    data=data.split("\t")
+    if name==data[0] and place==data[2]:
+        print("successfully logged in..")
+        break
+    else:
+        print("not placed try again..")
+
